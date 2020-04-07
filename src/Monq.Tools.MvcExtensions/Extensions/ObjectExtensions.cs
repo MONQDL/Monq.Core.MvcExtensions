939a41cf71234ae70fdc049f2fd10c6f1967ac84
NorthRebel <zatinasever@gmail.com>
2020-04-07 22:44:48 +0300

    Добавлены методы расширения для Enum и Object.

---
+++ src/Monq.Tools.MvcExtensions/Extensions/ObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Monq.Tools.MvcExtensions.Extensions
+{
+    /// <summary>
+    /// Методы расширения для работы с объектами.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Проверка на Null или DBNull.
+        /// </summary>
+        /// <param name="value">Тестируемое значение.</param>
+        /// <returns>True если объект Null or DBNull.</returns>
+        public static bool IsNull(this object value) => 
+            value == null || value == DBNull.Value;
+    }
+}
