d4351b2487de4227aef7617c74684da1f4aded3d
NorthRebel <zatinasever@gmail.com>
2020-08-10 14:18:48 +0300

    🔨 Реализован отдельный метод (хелпер), который возвращает выражение фильтрации определенного поля модели, значения которого входят в заданный диапазон (v2.2.1). - 🔖 v2.2.2

--- src/Monq.Tools.MvcExtensions/Extensions/QueryableExtensions.cs
+++ src/Monq.Tools.MvcExtensions/Extensions/QueryableExtensions.cs
@@ -63,7 +63,7 @@ namespace Monq.Tools.MvcExtensions.Extensions
         /// что в свою очередь дает прирост производительности в запросах.
         /// </summary>
         /// <typeparam name="TKey">Тип поля к которому будет выполнено построение предложения IN (...).</typeparam>
-        /// <typeparam name="TQuery">Основная сущность в запросе.</typeparam>
+        /// <typeparam name="T">Основная сущность в запросе.</typeparam>
         /// <param name="queryable">Запрос.</param>
         /// <param name="values">Список значение, которые будут переданы в условное выражение в пакетном режиме.</param>
         /// <param name="keySelector">Указатель поля к которому будет выполнено построение предложения IN (...).</param>
@@ -77,81 +77,13 @@ namespace Monq.Tools.MvcExtensions.Extensions
         /// https://gist.github.com/kroymann/e57b3b4f30e6056a3465dbf118e5f13d
         /// </para>
         /// </remarks>
-        public static IQueryable<TQuery> In<TKey, TQuery>(
-            this IQueryable<TQuery> queryable,
+        public static IQueryable<T> In<TKey, T>(
+            this IQueryable<T> queryable,
             IEnumerable<TKey> values,
-            Expression<Func<TQuery, TKey>> keySelector)
+            Expression<Func<T, TKey>> keySelector)
         {
-            if (values == null)
-                throw new ArgumentNullException(nameof(values));
-
-            if (keySelector == null)
-                throw new ArgumentNullException(nameof(keySelector));
-
-            if (!values.Any())
-                return queryable.Take(0);
-
-            var distinctValues = Bucketize(values);
-
-            if (distinctValues.Length > 2048)
-                throw new ArgumentException("Too many parameters for SQL Server, reduce the number of parameters",
-                    nameof(keySelector));
-
-            var predicates = distinctValues
-                .Select(v =>
-                {
-                    // Create an expression that captures the variable so EF can turn this into a parameterized SQL query
-                    Expression<Func<TKey>> valueAsExpression = () => v;
-                    return Expression.Equal(keySelector.Body, valueAsExpression.Body);
-                })
-                .ToList();
-
-            while (predicates.Count > 1)
-                predicates = PairWise(predicates)
-                    .Select(p => Expression.OrElse(p.Item1, p.Item2))
-                    .ToList();
-
-            var body = predicates.Single();
-
-            var clause = Expression.Lambda<Func<TQuery, bool>>(body, keySelector.Parameters);
-
+            var clause = ExpressionHelpers.GetExpressionToFilterByInClause(keySelector, values);
             return queryable.Where(clause);
-        }
-
-        /// <summary>
-        /// Break a list of items tuples of pairs.
-        /// </summary>
-        static IEnumerable<(T, T)> PairWise<T>(this IEnumerable<T> source)
-        {
-            using var sourceEnumerator = source.GetEnumerator();
-        
-            while (sourceEnumerator.MoveNext())
-            {
-                var a = sourceEnumerator.Current;
-                sourceEnumerator.MoveNext();
-                var b = sourceEnumerator.Current;
-
-                yield return (a, b);
-            }
-        }
-
-        static TKey[] Bucketize<TKey>(IEnumerable<TKey> values)
-        {
-            var distinctValueList = values.Distinct().ToList();
-
-            // Calculate bucket size as 1,2,4,8,16,32,64,...
-            var bucket = 1;
-            while (distinctValueList.Count > bucket) 
-                bucket *= 2;
-
-            // Fill all slots.
-            var lastValue = distinctValueList.Last();
-            
-            for (var index = distinctValueList.Count; index < bucket; index++) 
-                distinctValueList.Add(lastValue);
-
-            var distinctValues = distinctValueList.ToArray();
-            return distinctValues;
         }
     }
 }
